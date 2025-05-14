36677d5e4edbf171b65f9df172ded64becf4b708
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-05-14 14:34:20 +0900

    :tada: Create process to display days of a month
    Dateクラスを使い、今月(2025年5月)の1日から最終日までの日付を表示する。最終日にくるまで、next_dayメソッドを使って日付を追加し表示させる。

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -1,3 +1,10 @@
 #!/usr/bin/env ruby
+require 'date'
 
-puts "ruby calendar"
+start_date = Date.new(2025, 5, 1)
+end_date = Date.new(2025, 5, -1)
+
+while start_date <= end_date
+    print start_date.day, "  "
+    start_date = start_date.next_day
+end
