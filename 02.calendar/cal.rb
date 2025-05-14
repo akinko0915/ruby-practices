81a070238b7c1f2fb7c8b4cb0904069cfec7c7c3
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-05-14 15:34:00 +0900

    :sparkles: Format display of dates
    日付が土曜日の時に改行。日付の位置を揃えるために日付が1桁の時には、日付の前にスペースを空ける

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,6 +5,20 @@ start_date = Date.new(2025, 5, 1)
 end_date = Date.new(2025, 5, -1)
 
 while start_date <= end_date
-    print start_date.day, "  "
+    current_day = start_date.day
+    current_date = start_date
+
+    formatted_output =
+        if current_day < 10
+            " #{current_day}" # 一桁の場合、前にスペースを追加
+        else
+            print "#{current_day}"
+        end
+
+    if current_date.saturday?
+        puts formatted_output #改行
+    else
+        print formatted_output, "  "
+    end
     start_date = start_date.next_day
 end
