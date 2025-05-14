d9c743098ffe273af37ad89fc249e755f7d94467
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-05-14 16:26:24 +0900

    :sparkles: Format positions by day of the week
    曜日の列にその日付が並ぶように表示するために、週ごとに配列を作った。wdayメソッドを使って出た値と同じ配列のインデックス位置要素に日付を挿入する。
    土曜日まで行ったら、その週の要素を２ペース間隔で表示させ、新しい週の配列を使う。

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,22 +3,26 @@ require 'date'
 
 start_date = Date.new(2025, 5, 1)
 end_date = Date.new(2025, 5, -1)
+calendar_week = Array.new(7, "  ") #週ごとに配列を作る
 
 while start_date <= end_date
     current_day = start_date.day
     current_date = start_date
+    wday_position = current_date.wday
 
     formatted_output =
         if current_day < 10
             " #{current_day}" # 一桁の場合、前にスペースを追加
         else
-            print "#{current_day}"
+            "#{current_day}"
         end
 
-    if current_date.saturday?
-        puts formatted_output #改行
-    else
-        print formatted_output, "  "
+    calendar_week[wday_position] = formatted_output
+
+    if wday_position == 6   # 6=土曜日
+        puts calendar_week.join("  ") #これまでに挿入してきた要素の値を2つのスペースを空けて出力する
+        calendar_week = Array.new(7, "  ") #次の週に行くため、配列を初期化する
     end
+
     start_date = start_date.next_day
 end
