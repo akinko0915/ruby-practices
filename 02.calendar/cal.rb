6eec7318cd65cf4ba600a619f4ea7341660d96e8
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-05-14 16:44:26 +0900

    :sparkles: Display year and month
    曜日の名前を配列にして、２スペース間隔で表示する。
    年と月を文字列に変換して、カレンダーの真ん中で表示させる。

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,6 +5,11 @@ start_date = Date.new(2025, 5, 1)
 end_date = Date.new(2025, 5, -1)
 calendar_week = Array.new(7, "  ") #週ごとに配列を作る
 
+month_year = start_date.month.to_s + "月" + "　" + start_date.year.to_s
+puts month_year.center(25)
+week_name = ["日", "月", "火", "水", "木", "金", "土"]
+puts week_name.join("  ")
+
 while start_date <= end_date
     current_day = start_date.day
     current_date = start_date
