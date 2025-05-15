515d26fce361b3dbf475ed10c0afd92a86414197
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-05-15 09:15:59 +0900

    :recycle: Create function for formatting days
    1桁と2桁の時のフォーマットを調整する関数を作る

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -19,23 +19,27 @@ start_date = Date.new(year, month, 1)
 end_date = Date.new(year, month, -1)
 calendar_week = Array.new(7, "  ") #週ごとに配列を作る
 
-month_year = start_date.month.to_s + "月" + "　" + start_date.year.to_s
-puts month_year.center(25)
-week_name = ["日", "月", "火", "水", "木", "金", "土"]
-puts week_name.join("  ")
-
-while start_date <= end_date
-    current_day = start_date.day
-    current_date = start_date
-    wday_position = current_date.wday
-
+def format_day(current_day)
     formatted_output =
         if current_day < 10
             " #{current_day}" # 一桁の場合、前にスペースを追加
         else
             "#{current_day}"
         end
+end
 
+month_year = start_date.month.to_s + "月" + "　" + start_date.year.to_s
+puts month_year.center(25)
+week_name = ["日", "月", "火", "水", "木", "金", "土"]
+puts week_name.join("  ")
+
+
+while start_date <= end_date
+    current_day = start_date.day
+    current_date = start_date
+    wday_position = current_date.wday
+
+    formatted_output = format_day(current_day)
     calendar_week[wday_position] = formatted_output
 
     if wday_position == 6   # 6=土曜日
