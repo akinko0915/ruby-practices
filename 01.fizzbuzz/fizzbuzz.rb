9b437506706db0cfbc2feb212807eb41a112f381
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-05-19 09:05:13 +0900

    :shirt: Fix formatting
    インデントとスペースを修正。改行して表示されるようにした。

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,11 @@
 1.upto(20) do |i|
-    if i%3 == 0 && i%5 == 0
-        print "FizzBuzz", "  "
-    elsif i%3 == 0
-        print "Fizz", "  "
-    elsif i%5 == 0
-        print "Buzz", "  "
-    else
-        print i, "  "
-    end
+  if (i % 3 == 0) && (i % 5 == 0)
+    puts 'FizzBuzz'
+  elsif i % 3 == 0
+    puts 'Fizz'
+  elsif i % 5 == 0
+    puts 'Buzz'
+  else
+    puts i
+  end
 end
