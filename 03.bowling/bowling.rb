705e8d27b86f16da5f86a4402b011f3f96e9969a
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-05-19 11:55:44 +0900

    :bug: Fixed behavior of "X"
    ストライクの時に、[10, 0]となるように、配列に数字を挿入

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,9 +1,20 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
 # 1.引数をとる
 score = ARGV[0]
 # 2. 1投毎に分割する
 scores = score.split(',')
 # 3. 数字に変換
-frames = scores.map{ |score| score.to_i }
+shots = []
+scores.each do |s|
+  if s == 'X'
+    shots << 10
+    shots << 0
+  else
+    shots << s.to_i
+  end
+end
 # 4. フレーム毎に分割
-p frames.each_slice(2).to_a
+frames = shots.each_slice(2).to_a
+p frames
