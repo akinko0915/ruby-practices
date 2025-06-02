fb933191aa01feca5e339348af9fa23342a870af
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-06-02 12:22:08 +0900

    :recycle: Simplified codes and reduce variables

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,25 +1,17 @@
 #!/usr/bin/env ruby
 def display_table(rows, column_width)
   rows.each do |row|
-    row.each_with_index do |item, col_index|
-      print item.ljust(column_width[col_index] + 2)
-    end
+    print row.each_with_index.map {|item, col_index| item.ljust(column_width[col_index] + 2)}.join
     puts
   end
 end
 
 def format_table(contents, col_count)
   row_count = (contents.size.to_f / col_count).ceil
-  sorted_contents = contents.sort
-  columns = sorted_contents.each_slice(row_count).to_a;
-  rows = columns.each{ |col| col[row_count - 1] = " " if col.length < row_count }.transpose
-  column_width = columns.map do |col|
-    length_array = col.map do |c|
-      c.length
-    end
-    length_array.max
-  end
-  [rows, column_width]
+  columns = contents.sort.each_slice(row_count).map { |col| col.fill("", col.size...row_count)}
+  rows = columns.transpose
+  col_widths = columns.map { |col| col.map(&:length).max }
+  [rows, col_widths]
 end
 
 def display_directory_listing(col_count = 3)
