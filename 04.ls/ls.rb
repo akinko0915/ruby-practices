219877acc3dd12ca466dd2efae2ab206bb9f463f
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-06-02 10:35:08 +0900

    :sparkles: Display all contents in the correct order

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,4 +4,22 @@ current_directory = Dir.pwd
 
 # 2. ディレクトリ内容取得機能を実装
 contents = Dir.children(current_directory)
-puts contents
+row_number = 3
+
+# 3. 表示形式を整える
+def format_columns(contents, column_number)
+  # 要素数から行数を計算
+  row_count = (contents.size.to_f / column_number).ceil
+  sorted_contents = contents.sort
+
+  # 縦(列)で分割
+  columns = sorted_contents.each_slice(row_count).to_a;
+  # 横 (行)で分割
+  rows = columns.transpose
+
+  rows.each do |row|
+    puts row.join(" ")
+  end
+end
+
+format_columns(contents, row_number);
