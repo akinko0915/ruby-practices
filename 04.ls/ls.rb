5ac764e2038e91d32bed8a74c2eebd16e69391d5
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-06-10 16:08:16 +0900

    :bug: Extract all entries

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -23,7 +23,7 @@ end
 
 def display_filenames(col_count, options)
   current_directory = Dir.pwd
-  contents = Dir.children(current_directory).sort
+  contents = Dir.entries(current_directory).sort
   contents = contents.reject { |name| name.start_with?('.') } unless options[:a]
   rows, col_widths = format_as_table(contents, col_count)
   display_filenames_table(rows, col_widths)
