ec6c1d9d4582af3910b8df35011013f7b0af61d0
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-06-16 11:54:58 +0900

    :recycle: Move total_score method to frame class

--- 06.bowling_object/frame.rb
+++ 06.bowling_object/frame.rb
@@ -16,4 +16,18 @@ class Frame
   def spare?
     @shots.sum == 10
   end
+
+  def bonus_score(frame1, frame2)
+    score = 0
+    if strike?
+      if frame1.strike? && frame2
+        score = 10 + frame2.shots[0]
+      else
+        score = frame1.shots.sum
+      end
+    elsif spare?
+      score = frame1.shots[0]
+    end
+    score
+  end
 end
--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -1,6 +1,5 @@
 require_relative './frame'
 require_relative './shot'
-
 class Game
   def initialize(raw_scores)
     shots = raw_scores.map { |s| Shot.new(s).score }.flatten
@@ -11,27 +10,12 @@ class Game
     total = 0
     @frames.each_with_index do |frame, i|
       total += frame.sum
-      if i < 9
-        total += bonus_score(frame, i)
+      next_frame = @frames[i + 1]
+      next_next_frame = @frames[i + 2]
+      if (i < 9) && (frame.strike? || frame.spare?)
+        total += frame.bonus_score(next_frame, next_next_frame)
       end
     end
     total
-  end
-
-  private
-
-  def bonus_score(frame, index)
-    next_frame = @frames[index + 1]
-    score = 0
-    if frame.strike?
-      if next_frame.strike? && @frames[index + 2]
-        score = 10 + @frames[index + 2].shots[0]
-      else
-        score = next_frame.shots.sum
-      end
-    elsif frame.spare?
-      score = @frames[index + 1].shots[0]
-    end
-    score
   end
 end
