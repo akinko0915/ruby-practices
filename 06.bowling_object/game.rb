fe919811dd277867462708b5850c128ae94779fe
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-06-17 13:28:24 +0900

    :+1: Create shot score array in building frames

--- 06.bowling_object/game.rb
+++ 06.bowling_object/game.rb
@@ -5,23 +5,24 @@ require_relative './shot'
 class Game
   FRAME_COUNT = 10
   def initialize(raw_scores)
-    shots = raw_scores.map { |s| Shot.new(s).score }
+    shots = raw_scores.map { |s| Shot.new(s) }
     @frames = build_frames(shots)
   end
 
   def build_frames(shots)
+    shot_scores = shots.map{ |shot| shot.score }
     frames = []
     count = 0
     (FRAME_COUNT - 1).times do
-      if shots[count] == 10
+      if shot_scores[count] == 10
         frames << Frame.new([10])
         count += 1
       else
-        frames << Frame.new([shots[count], shots[count + 1]])
+        frames << Frame.new([shot_scores[count], shot_scores[count + 1]])
         count += 2
       end
     end
-    frames << Frame.new(shots[count..])
+    frames << Frame.new(shot_scores[count..])
     frames
   end
 
