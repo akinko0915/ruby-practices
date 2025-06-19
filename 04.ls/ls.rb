df83b49d21ce5392afdcf86bb0bd089399afb574
akinko0915 <114070658+akinko0915@users.noreply.github.com>
2025-06-19 14:53:29 +0900

    :art: Format file data output

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -30,7 +30,7 @@ def display_filenames(col_count, options)
   contents = contents.reject { |name| name.start_with?('.') } unless options[:a]
   if options[:l]
     total_blocks = 0
-    outputs = []
+    file_data = []
     contents.map do |file|
       file_info = File::Stat.new(file)
       mode = mode_to_symbolic(file_info.mode.to_s(8).rjust(6, '0'))
@@ -43,11 +43,38 @@ def display_filenames(col_count, options)
       month = mtime.month
       day = mtime.day
       time = "#{mtime.hour}:#{mtime.min}"
-      all_info = [mode, nlink, owner_name, group_name, size, month, day, time, file].join(" ")
-      outputs << all_info
+      file_data << {mode:, nlink:, owner_name:, group_name:, size:, month:, day:, time:, file:}
     end
+
+    max_width = {
+      nlink: file_data.map{|d| d[:nlink].to_s.size }.max,
+      owner_name: file_data.map{|d| d[:owner_name].to_s.size }.max,
+      group_name: file_data.map{|d| d[:group_name].to_s.size }.max,
+      size: file_data.map{|d| d[:size].to_s.size }.max,
+      month: file_data.map{|d| d[:month].to_s.size }.max,
+      day: file_data.map{|d| d[:day].to_s.size }.max,
+      time: file_data.map{|d| d[:time].to_s.size }.max,
+      file: file_data.map{|d| d[:file].to_s.size }.max,
+    }
+
+    puts max_width
     puts "total #{total_blocks}"
-    puts outputs
+
+    file_data.each do |f|
+      puts [
+        f[:mode],
+        f[:nlink].to_s.rjust(max_width[:nlink]),
+        f[:owner_name].rjust(max_width[:owner_name]),
+        f[:group_name].rjust(max_width[:group_name]),
+        f[:size].to_s.rjust(max_width[:size]),
+        f[:month].to_s.rjust(max_width[:month]),
+        f[:day].to_s.rjust(max_width[:day]),
+        f[:time].rjust(max_width[:time]),
+        f[:file].rjust(max_width[:file])
+    ].join(' ')
+    end
+
+    return
   end
   rows, col_widths = format_as_table(contents, col_count)
   display_filenames_table(rows, col_widths)
